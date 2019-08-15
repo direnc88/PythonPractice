fcb258219316e8a630d2075d2e68f13da185fd2d
direnc88 <direnc88@users.noreply.github.com>
2019-08-14 20:46:58 -0500

    Create addToGameInventory.py
    created base code for addToGameInventory. need to complete code still.

---
+++ ch.5/addToGameInventory.py
@@ -0,0 +1,8 @@
+def addToInventory(inventory, addedItems):
+    #code goes here
+    
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
