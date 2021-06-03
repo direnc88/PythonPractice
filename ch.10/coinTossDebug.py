ca9a2a66a07fe703cb143488a949cbe02c9de0d2
direnc88 <direnc88@users.noreply.github.com>
2021-06-02 21:08:00 -0500

    Update coinTossDebug.py
    fixed the errors and added a bit of logging statements.

--- ch.10/coinTossDebug.py
+++ ch.10/coinTossDebug.py
@@ -6,18 +6,31 @@
 #from running.
 
 import random, logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - ' '%(levelname)s - %(message)s') 
+
+logging.debug('Start of program')
 
 guess = ''
 while guess not in ('heads', 'tails'):
-    print('Guess the coin toss! enter heads or tails:')
+    logging.debug('Start of guess')
+    print('Guess the coin toss! enter heads or tails: ')
     guess = input()
+
+logging.debug('Start of coin toss')
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+
+#this was added. Needed to turn the 0 and the 1 to heads and tails. 
+if toss == 0:
+    toss = 'tails'
+if toss == 1:
+    toss = 'heads'
+    
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()   #there are 3 s' instead of two.
-    if toss = guess:
+    guess = input()   #there are 3 s' instead of two.
+    if toss == guess:  #there was one equals symbol here instead of two. 
         print('You got it!')
     else:
         print('Nope. you are really bad at this game.')
