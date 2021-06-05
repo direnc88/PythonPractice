f0b08183797dbb4dec1c1f40777db1b53fba48e4
direnc88 <direnc88@users.noreply.github.com>
2021-06-05 15:31:44 -0500

    Update lucky.py
    finished the lucky project in chapter 11 ATBS. This should be usable from the command line. Use 'lucky' and a search term to use it.

--- ch.11/lucky.py
+++ ch.11/lucky.py
@@ -13,6 +13,14 @@ res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 #TODO: Retrieve top search result links.
+#retrieve top search result links
+soup = bs4.BeautifulSoup(res.text)
 
 #TODO: Open a broswer tab for each result.
+#Open a browser tab fro each result
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkelems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
+
 
