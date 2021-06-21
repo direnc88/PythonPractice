06d1b54bd90f21fbf796bc540f161cc138539c52
direnc88 <direnc88@users.noreply.github.com>
2021-06-21 16:27:43 -0500

    created multiplicationTableMaker

---
+++ ch.12/multiplicationTableMaker.py
@@ -0,0 +1,9 @@
+#! python 3
+
+# multiplicationTableMaker.py
+
+#this program is to take a number from the user and make a
+#multiplication table of that number in excel.Bold the first column and row.
+
+def multiTableMaker():
+    
