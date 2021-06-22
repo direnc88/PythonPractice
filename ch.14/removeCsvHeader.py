25ee1a63aa538777b56d39309fd8eef98c208a00
direnc88 <direnc88@users.noreply.github.com>
2021-06-22 14:56:32 -0500

    Update removeCsvHeader.py
    added todos

--- ch.14/removeCsvHeader.py
+++ ch.14/removeCsvHeader.py
@@ -6,3 +6,16 @@
 import csv, os
 os.makedirs('headerRemoved', exist_ok = True)
 
+#Loop thorugh every file in the current working directory.
+for csvFilename in os.listdir('.'):
+    if not csvFilename.endswith('.csv'):
+        countinue #skip non-csv files
+
+
+    print('Removing header from ' + csvFilename + '...')
+
+    #TODO: Read the CSV file in (skipping first row).
+
+    #TODO: Write out the CSV file.
+
+    
