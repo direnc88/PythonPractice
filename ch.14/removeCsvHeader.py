2fcdab52f5ae34984f1350db4b093aa0694ff2ec
direnc88 <direnc88@users.noreply.github.com>
2021-06-25 16:41:39 -0500

    Update removeCsvHeader.py
    added the feature of reading in csv files and writing out the csv files.

--- ch.14/removeCsvHeader.py
+++ ch.14/removeCsvHeader.py
@@ -15,7 +15,21 @@ for csvFilename in os.listdir('.'):
     print('Removing header from ' + csvFilename + '...')
 
     #TODO: Read the CSV file in (skipping first row).
+    csvRows = []
+    csvFileObj = open(csvFilename)
+    readerObj = csv.reader(csvFileObj)
+    for row in readerObj:
+        if readerObj.line_num == 1:
+            continue #skip first row
+        csvRows.append(row)
+    csvFileObj.close()
 
+    
     #TODO: Write out the CSV file.
-
+    csvFileObj = open(os.path.join('headerRemoved', csvFilename), 'w',
+                      newline='')
+    csvWriter = csv.writer(csvFileObj)
+    for row in csvRows:
+        csvWriter.writerow(row)
+    csvFileObj.close()
     
