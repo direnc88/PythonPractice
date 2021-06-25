8bd31f30f109a6290f56e7b9a5cf52ad6927c0c0
direnc88 <direnc88@users.noreply.github.com>
2021-06-25 16:53:27 -0500

    Create quickWeather.py
    created quickWeather.py and also added the first todo list item: download the JSON data from OpenWeatherMap.org's API.

---
+++ ch.14/quickWeather.py
@@ -0,0 +1,20 @@
+#! python 3
+
+#quickWeather.py - prints the wether for a locaiton from the command line.
+
+import json, reqeusts, sys
+
+#compute location from command line arguments.
+if len(sys.argv) < 2:
+    print('Usage: quickWeather.py location')
+    sys.exit()
+location = ' '.join(sys.argv[1:])
+
+#TODO: download the JSON data from OpenWeatherMap.org's API.
+url = 'http://api.openweathermap.org/data/2.5/forcast/daily?q=%s&cnt=3' % (location)
+response = requests.get(url)
+response.raise_for_status()
+
+
+#TODO: Load JSONdata into a python variable.
+
