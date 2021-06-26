c93764db353e4818839879f302ae68b5b6991c4f
direnc88 <direnc88@users.noreply.github.com>
2021-06-25 20:35:29 -0500

    Update stopwatch.py

--- ch.15/stopwatch.py
+++ ch.15/stopwatch.py
@@ -10,3 +10,18 @@ print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. P
 input()    #press ENTER to begin
 
 print('Started.')
+startTime = time.time() #get the first lap's start time.
+lastTime = startTime
+lapNum = 1
+
+#TODO: start tracking lap times.
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end ='')
+        lapNum += 1
+        
+
+
