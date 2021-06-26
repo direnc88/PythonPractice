6276cb74aa0e664c3d4ffbc2f618871dda8c27ce
direnc88 <direnc88@users.noreply.github.com>
2021-06-26 18:19:22 -0500

    Update downloadXkcd.py
    edited out some old code, commented out old code, and added new todo statements.

--- ch.11/downloadXkcd.py
+++ ch.11/downloadXkcd.py
@@ -10,7 +10,7 @@
 
 #repeats until it reaches the first comic.
 
-import requests, os, bs4
+import requests, os, bs4, threading
 
 url = 'http://xkcd.com'    # starting url
 os.makedirs('xkcd', exist_ok = True) #store comics in ./xkcd
@@ -26,27 +26,28 @@ while not url.endswith('#'):
 
 
     #TODO: Find the URL of the comic image
-    #find the URL of the comic image.
     comicElem = soup.select('#comic image')
     if comicElem == []
         print('Could not find comic image.')
     else:
         comicUrl = 'http:' + comicElem[0].get('src')
     #TODO: download the image
-        #download the image
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
     #TODO: save the image to ./xkcd.
-    #save the image to ./xkcd
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
         for chuck in res.iter_content(100000):
             imageFile.write(chunk)
-        imageFile.close() 
+        imageFile.close()
+        
     #TODO: get the prev button's url.
-    #get the prev button's url.
-    prevLink = soup.select('a[rel="prev"]')[0]
-    url = 'http://xkcd.com' + prevLink.get('href')
+    #prevLink = soup.select('a[rel="prev"]')[0]
+    #url = 'http://xkcd.com' + prevLink.get('href')
 
-print('Done.)
+#print('Done.)
+
+#TODO: Create and start the Thread objects.
+
+#TODO: Wait for all threads to end. 
 
