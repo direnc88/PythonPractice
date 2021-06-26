9e8cc823534f1e897c0c0d9a0f797dfb7e40fb6e
direnc88 <direnc88@users.noreply.github.com>
2021-06-25 19:34:34 -0500

    Create stopwatch.py

---
+++ ch.15/stopwatch.py
@@ -0,0 +1,12 @@
+#! python 3
+
+#stopwatch.py - A simple stopwatch program.
+
+import time
+
+#display the program's instructions
+print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. Press Ctrl-C to quit')
+
+input()    #press ENTER to begin
+
+print('Started.')
