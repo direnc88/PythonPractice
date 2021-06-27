b143865439c8fa0aab94848e1a917fe1332498e7
direnc88 <direnc88@users.noreply.github.com>
2021-06-26 21:42:50 -0500

    Update multidownloadXkcd.py
    cleaned this up a bit.

--- ch.15/multidownloadXkcd.py
+++ ch.15/multidownloadXkcd.py
@@ -15,9 +15,9 @@ import requests, os, bs4
 url = 'http://xkcd.com'    # starting url
 os.makedirs('xkcd', exist_ok = True) #store comics in ./xkcd
 
-while not url.endswith('#'):
+def downloadXkcd(startComic, endComic):
+    #while not url.endswith('#'):
     #TODO: download the page
-    #download the page
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
@@ -31,17 +31,25 @@ while not url.endswith('#'):
         print('Could not find comic image.')
     else:
         comicUrl = 'http:' + comicElem[0].get('src')
-    #TODO: download the image
+        
+        #TODO: download the image
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
-    #TODO: save the image to ./xkcd.
+        
+        #TODO: save the image to ./xkcd.
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
         for chuck in res.iter_content(100000):
             imageFile.write(chunk)
-        imageFile.close() 
+        imageFile.close()
+        
     #TODO: get the prev button's url.
-    prevLink = soup.select('a[rel="prev"]')[0]
-    url = 'http://xkcd.com' + prevLink.get('href')
+    #prevLink = soup.select('a[rel="prev"]')[0]
+    #url = 'http://xkcd.com' + prevLink.get('href')
 
-print('Done.)
+#print('Done.)
+
+#TODO: Create and start the Thread Objects.
+
+#TODO: Wait for all threads to end.
+
