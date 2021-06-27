43d69ad7bf8f1924073066e6323aafd34cf60a9b
direnc88 <direnc88@users.noreply.github.com>
2021-06-26 19:01:14 -0500

    Update downloadXkcd.py
    fixed some errors I had made.

--- ch.11/downloadXkcd.py
+++ ch.11/downloadXkcd.py
@@ -17,7 +17,6 @@ os.makedirs('xkcd', exist_ok = True) #store comics in ./xkcd
 
 while not url.endswith('#'):
     #TODO: download the page
-    #download the page
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
@@ -31,23 +30,22 @@ while not url.endswith('#'):
         print('Could not find comic image.')
     else:
         comicUrl = 'http:' + comicElem[0].get('src')
-    #TODO: download the image
+        #TODO: download the image
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
-    #TODO: save the image to ./xkcd.
+        
+        #TODO: save the image to ./xkcd.
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
         for chuck in res.iter_content(100000):
             imageFile.write(chunk)
         imageFile.close()
         
     #TODO: get the prev button's url.
-    #prevLink = soup.select('a[rel="prev"]')[0]
-    #url = 'http://xkcd.com' + prevLink.get('href')
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prevLink.get('href')
 
-#print('Done.)
+print('Done.)
 
-#TODO: Create and start the Thread objects.
 
-#TODO: Wait for all threads to end. 
 
