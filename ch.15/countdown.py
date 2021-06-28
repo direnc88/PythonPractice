52ffc8dfbe28e95e17fffc07fbf356666a0a388c
direnc88 <direnc88@users.noreply.github.com>
2021-06-28 12:04:12 -0500

    Update countdown.py
    added decrementing to the countdown file.

--- ch.15/countdown.py
+++ ch.15/countdown.py
@@ -2,3 +2,14 @@
 
 #countdown.py - A simple countdown script
 
+import time, subprocess
+
+timeLeft = 60
+while timeLeft > 0:
+    print(timeLeft, end='')
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+
+
+#TODO: At the end fo the countdown, play a sound file.
+
