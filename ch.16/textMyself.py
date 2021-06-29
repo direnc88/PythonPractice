52e64de0939dcca7eab855ad9a7621534ed82b86
direnc88 <direnc88@users.noreply.github.com>
2021-06-29 17:01:07 -0500

    Update textMyself.py
    added the function to text myself.

--- ch.16/textMyself.py
+++ ch.16/textMyself.py
@@ -1,1 +1,16 @@
 #! python 3
+
+#textMyself.py - Defines the textmyself() function that texts a message
+#passed to it as a string.
+
+#Preset values:
+accountSID = 'ACxxxxxxxxxxxxxxxxxxxxxxx'
+authToken = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+myNumber = '+15559998888'   #this will be different.
+twilionumber = '+15552225678'  #this will be different.
+
+from twilio.rest import TwilioRestClient
+
+def textmyself(message):
+    twilioCli = twilioRestClient(accountSID, authToken)
+    twilioCli.messages.create(body=message, from_=twilioNumber, to = myNumber)
