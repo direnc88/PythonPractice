4cefa3a0f59666170cb88d5f246a42ffd8ec150c
direnc88 <direnc88@users.noreply.github.com>
2021-06-30 16:59:04 -0500

    Update resizeAndAddLogo.py
    added todo statements.

--- ch.17/resizeAndAddLogo.py
+++ ch.17/resizeAndAddLogo.py
@@ -4,4 +4,23 @@
 # in a 300x300 square, and adds catlogo.png to the lower-right corner.
 
 import os
+from PIL import image
+
+SQUARE_FIT_SIZE = 300
+LOGO_FILENAME = 'catlogo.png'
+
+logoIm = Image.open(LOGO_FILENAME)
+logoWidth, logoHeight = logoIm.size
+
+#TODO: Loop over all files in the working directory.
+
+#TODO: Check if image nees to be resized.
+
+#TODO: Caculate the new width and height to resize to.
+
+#TODO: Resize the image.
+
+#TODO: Add the logo.
+
+#TODO: Save changes.
 
