34c93138d3c00fa1dc18e80e14ae4131644d6be4
direnc88 <direnc88@users.noreply.github.com>
2021-07-05 18:12:32 -0500

    Update mouseNow.py

--- ch.18/mouseNow.py
+++ ch.18/mouseNow.py
@@ -8,3 +8,17 @@ print('Press Ctrl-C to quit.')
 
 #TODO: get and print the mouse coordinates.
 
+try:
+    while True:
+        #TODO: Get and print the mouse coordinates
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + 'Y: ' + str(y).rjust(4)
+except KeyboardInterrupt:
+    print('\nDone.')
+    print(positionStr, end='')
+    print('\b' * len(positionStr), end='', flush=True)
+
+
+
+
+
