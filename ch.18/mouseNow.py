83b7ada6995dea58f3f70c6df1b97088c09b8934
direnc88 <direnc88@users.noreply.github.com>
2021-07-06 16:38:04 -0500

    Extended mouseNow
    I extended mouseNowExtended and I made a correciton to MouseNow as well.

--- ch.18/mouseNow.py
+++ ch.18/mouseNow.py
@@ -13,10 +13,12 @@ try:
         #TODO: Get and print the mouse coordinates
         x, y = pyautogui.position()
         positionStr = 'X: ' + str(x).rjust(4) + 'Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        
 except KeyboardInterrupt:
     print('\nDone.')
-    print(positionStr, end='')
-    print('\b' * len(positionStr), end='', flush=True)
+
 
 
 
