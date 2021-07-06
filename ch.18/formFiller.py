c4db29b749c9acf76e0a31949a69fba1efa38247
direnc88 <direnc88@users.noreply.github.com>
2021-07-06 17:10:51 -0500

    Update formFiller.py
    filled out formData variable.

--- ch.18/formFiller.py
+++ ch.18/formFiller.py
@@ -4,3 +4,36 @@
 
 import pyautogui, time
 
+#Set these to the correct coordinates for your computer.
+nameField = (648, 319)
+submitButton = (651, 817)
+submitButtonColor = (75, 141, 249)
+submitAnotherLink = (760, 224)
+
+formData = [{'name': 'Alice', 'fear': 'eavesdroppers', 'source': 'wand', 'robocop': 4, 'comments': 'Tell Bob I said hi.'},
+            {'name': 'Bob', 'fear': 'bees', 'source': 'amulet', 'robocop': 4, 'comments': 'n/a'},
+            {'name': 'Carol', 'fear': 'puppets', 'source': 'crystal ball', 'robocop': 1, 'comments': 'Please take the puppets out of the break room.'},
+            {'name': 'Alex Murphy', 'fear': 'ED-209', 'source': 'money', 'robocop': 5, 'comments': 'Protect the innocent. Serve the public trust. Uphold the law.'}
+            ]
+             
+
+#TODO: Give the user a chance to kill the script.
+
+#TODO: Wait until the form page has loaded.
+
+#TODO: Fill out the Name Field.
+
+#TODO: FIll out the Greatest Fear(s) field.
+
+#TODO: Fill out the Source of Wizard Powers field.
+
+#TODO: Fill out the RoboCop field.
+
+#TODO: Fill out the Additional Comments field.
+
+#TODO: Click submit.
+
+#TODO: Wait until form page has loaded.
+
+#TODO: Click the Submit another response link.
+
